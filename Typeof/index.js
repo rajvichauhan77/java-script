d74c28e421568c1fea91496b547ca87b0b59ff50
rajvichauhan77 <chauhanrajvi77@gmail.com>
2024-12-02 17:32:00 +0530

    slikhyrfas

---
+++ Typeof/index.js
@@ -0,0 +1,24 @@
+// let num = 10
+// let num2 = String(num)
+
+// console.log(typeof num)
+// console.log(typeof num2)
+
+// console.log(num)
+// console.log(num2)
+
+
+
+
+// let arr = ["hello", 30, "world", 9]
+// console.log(typeof arr)
+// console.log(arr)
+
+
+let obj = {
+    name :"rajvi",
+    age : 19,
+   number : 99999999
+}
+console.log(obj)
+
---
+++ first/first.js
@@ -0,0 +1,9 @@
+
+console.log("hello...");
+
+var r = "hiiii";
+
+r=9;
+
+
+console.log(r);
