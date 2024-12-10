cc71d11a3999189e74753eb0062be3c66641c2db
rajvichauhan77 <chauhanrajvi77@gmail.com>
2024-12-10 18:19:54 +0530

    akertfgwelaiuyr

---
+++ Heigher order loops/foreach.js
@@ -0,0 +1,32 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+// let sum = arr.forEach(ele => {
+//     if(ele%2==0){
+//         return ele*ele;
+//     }
+// });
+
+
+// let num1 = arr.map((ele) => {
+//     return ele*ele
+// })
+
+
+// let num2 = arr.filter((ele) => {
+//     return ele > 6
+// })
+
+
+// console.log(num1)
+
+
+
+// let number = arr.map((ele) => ele*ele).filter((ele) => ele > 9)
+
+// console.log(number)
+
+
+
+let num = arr.every((ele) => ele >= 1)
+console.log(num)
